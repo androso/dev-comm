24f190659d5696defdc9cc51b9e7b737775c756e
androso <androso421@gmail.com>
2026-03-09 14:50:01 -0600

    Add product schema definitions for create, update, query, and parameters

---
+++ modules/products/product.schema.ts
@@ -0,0 +1,32 @@
+import { t } from "elysia";
+
+export const createProductSchema = t.Object({
+	name: t.String({ minLength: 1 }),
+	price: t.Number({ minimum: 0 }),
+	description: t.Optional(t.String({ minLength: 1 })),
+	sku: t.Optional(t.String()),
+	stockQuantity: t.Optional(t.Integer({ minimum: 0 })),
+	categoryId: t.Optional(t.String({ format: "uuid" })),
+    imageUrl: t.Optional(t.String({ maxLength: 2048 })),
+	isActive: t.Optional(t.Boolean()),
+	providerIds: t.Optional(t.Array(t.String({ format: "uuid"})))
+});
+
+export const updateProductSchema = t.Partial(createProductSchema)
+
+export const productQuerySchema = t.Object({
+	page: t.Optional(t.Numeric({ minimum: 1 })),
+	limit: t.Optional(t.Numeric({ minimum: 1, maximum: 100 })),
+	fields: t.Optional(t.String()),
+	name: t.Optional(t.String()),
+	isActive: t.Optional(t.BooleanString())
+})
+
+export const productParamsSchema = t.Object({
+	id: t.String({ format: "uuid"})
+})
+
+export type CreateProductPayload = typeof createProductSchema.static;
+export type UpdateProductPayload = typeof updateProductSchema.static;
+export type ProductQuery = typeof productQuerySchema.static;
+export type ProductParams = typeof productParamsSchema.static;
