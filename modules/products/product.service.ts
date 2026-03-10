30bf9de5d3cb3e14563374469feebf4695cc3f87
androso <androso421@gmail.com>
2026-03-10 11:12:57 -0600

    Implement product service with create and getById methods

---
+++ modules/products/product.service.ts
@@ -0,0 +1,27 @@
+import { productRepository } from "./product.repository";
+import { CreateProductPayload } from "./product.schema";
+
+export const productService = {
+	async create(data: CreateProductPayload) {
+		const newData = {
+			...data,
+			price: String(data.price),
+		};
+		const product = await productRepository.create(newData);
+
+		return {
+			...product,
+			price: parseFloat(product.price),
+		};
+	},
+	async getById(id: string) {
+		const product = await productRepository.findById(id);
+		if (!product) return null;
+
+		return {
+			...product,
+			price: parseFloat(product.price),
+			providers: product.productProvidersTable.map((pp) => pp.provider),
+		};
+	},
+};
